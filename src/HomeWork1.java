cc52fc949c41e1cd358168815be75d300eaaa889
Dushuk Oleksandr <dushuk@gmail.com>
2023-02-01 16:56:12 +0100

    add Homework

--- src/HomeWork1.java
+++ src/HomeWork1.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class HomeWork1 {
+public class HomeWork1 {
+
+    // сделать массив длиной 22
+    int [] array1= new int [22];
+
+    // обойти массив циклом и посчитать сумму элементов с начала и до конца
+    public static int getSumFor(int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    // обойти массив циклом и посчитать сумму элементов с конца в начало
+    public static int getSumBack(int[] arr){
+        int sum = 0;
+        for (int i = arr.length-1; i >=0; i--) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    // посчитать сумму элементов с четными, а потом с нечетными индексами
+    public static int getSumEven(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            if (i % 2 == 0) sum +=arr[i];
+        }
+        return sum;
+    }
+
+    //  посчитать сумму элементов с нечетными индексами
+    public static int getSumOdd(int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length ; i++) {
+            if (i % 2 != 0) sum += arr[i];
+        }
+        return sum;
+    }
+
+    // пройтись циклом так что бы суммировались первый и последний, потом второй и предпоследний итд
+    public static int getSum(int[] arr) {
+        int sum = 0;
+        int start = 0;
+        int end = arr.length -1;
+        while (start <= end) {
+            if(start == end){
+                sum = sum + arr[start];
+                break;
+            }
+            sum += (arr[start] + arr[end]);
+            start++;
+            end--;
+        }
+        return sum;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 public class Main {
-    public static void main(String[] args) {
 
-        int[] array2 = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int summ1 = 0;
-        for (int i = 0; i < array2.length; i++) {
-            summ1 += array2[i];
-        }
-        System.out.println(summ1);
+
+// HomeWork 1
+    public static void main(String[] args) {
+        //          0  1  2  3  4  5  6  7  8
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        // System.out.println(HomeWork1.getSumFor(a));
+
+        // System.out.println(HomeWork1.getSumBack(a));
+
+        // System.out.println(HomeWork1.getSumEven(a));
+
+        // System.out.println(HomeWork1.getSumOdd(a));
+
+        // System.out.println(HomeWork1.getSum(a));
+        
+
     }
 }
